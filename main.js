5b595ae8dd8b973079267c4a2a68b646820b98a8
Imran <imranshahadat95@gmail.com>
2021-09-12 22:21:11 +0600

    FInal Exam

---
+++ main.js
@@ -0,0 +1,19 @@
+function start() {
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+  let internetbill = document.getElementById("internet bill").value;
+  let deviceMfees = document.getElementById("deviceMfees").value;
+  let internetbillYearly = internetbill * 12;
+  let monthllydeviceFees = deviceMfees / 12;
+  let totalyearlyFees = internetbillYearly + deviceMfees;
+  let incentiveamount;
+  if (age > 50 && totalyearlyFees > 20000) {
+    incentiveamount = 0.2 * internetbillYearly;
+  } else if (age > 30 && monthllydeviceFees >= 250) {
+    incentiveamount = 0.15 * internetbillYearly;
+  }
+
+  let msg = ` Dear ${name} your incentive amount is ${incentiveamount} `;
+
+  document.getElementById("msg").innerHTML = msg;
+}
